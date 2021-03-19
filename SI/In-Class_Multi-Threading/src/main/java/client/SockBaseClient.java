bf808336c15b6d7c665069269de1700faf50623d
srfaye <srfaye@asu.edu>
2021-03-19 16:46:54 -0700

    Modified In-Class_Multi-Threading to demonstrate race conditions and how to avoid them

--- SI/In-Class_Multi-Threading/src/main/java/client/SockBaseClient.java
+++ SI/In-Class_Multi-Threading/src/main/java/client/SockBaseClient.java
@@ -7,62 +7,67 @@ import buffers.OperationProtos.Request;
 import buffers.OperationProtos.Response;
 
 class SockBaseClient {
-     
-
-     public static void main (String args[]) throws Exception {
-        Socket serverSock = null;
-        OutputStream out = null;
-        InputStream in = null;
-        int i1=0, i2=0;
-        int port = 9099; // default port
-
-        if (args.length != 3) {
-            System.out.println("Expected arguments: <host(String)> <port(int)> <data(json file)>");
-            System.exit(1);
-        }
-        String host = args[0];
-        try {
-            port = Integer.parseInt(args[1]);
-        } catch (NumberFormatException nfe) {
-            System.out.println("[Port] must be integer");
-            System.exit(2);
-        }
-        String filename = args[2];
-       
-
-        try {
-            // connect to the server
-            serverSock = new Socket(host, port);
-
-            // write to the server
-            out = serverSock.getOutputStream();
-
-            // read from the server
-            in = serverSock.getInputStream();
-
-            Request.Builder reqBuilder = Request.newBuilder();
-            reqBuilder.setOperationType(Request.OperationType.ADD);
-            reqBuilder.addNums(1);
-            reqBuilder.addNums(2);
-            reqBuilder.addNums(3);
-            reqBuilder.addNums(4);
-
-            Request req = reqBuilder.build();
-
-            req.writeDelimitedTo(out); // send off to server
-
-            Response res = Response.parseDelimitedFrom(in);
-            System.out.println("Resuls is: " + res.getResult());
 
 
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (in != null)   in.close();
-            if (out != null)  out.close();
-            if (serverSock != null) serverSock.close();
-        }
+  public static void main (String args[]) throws Exception {
+    Socket serverSock = null;
+    OutputStream out = null;
+    InputStream in = null;
+    int i1=0, i2=0;
+    int port = 8007; // default port
+
+    if (args.length != 3) {
+      System.out.println("Expected arguments: <host(String)> <port(int)> <data(json file)>");
+      System.exit(1);
     }
-}
+    String host = args[0];
+    try {
+      port = Integer.parseInt(args[1]);
+    } catch (NumberFormatException nfe) {
+      System.out.println("[Port] must be integer");
+      System.exit(2);
+    }
+    String filename = args[2];
 
+    try {
+      // connect to the server
+      serverSock = new Socket(host, port);
+
+      // write to the server
+      out = serverSock.getOutputStream();
+      // read from the server
+      in = serverSock.getInputStream();
+      BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+      while (true) {
+
+        System.out.println("Enter anything to send a message or 'quit' to exit...");
+        String userInput = stdin.readLine();
+
+        // quit the client program
+        if (userInput.equalsIgnoreCase("quit")) {
+          // close connection
+          serverSock.close();
+          break;
+        }
+
+        Request.Builder reqBuilder = Request.newBuilder();
+        reqBuilder.setOperationType(Request.OperationType.RACE_CONDITION_TEST);
+        reqBuilder.setStr(userInput);
+
+        Request req = reqBuilder.build();
+
+        req.writeDelimitedTo(out); // send off to server
+
+        // Receive the response
+        Response res = Response.parseDelimitedFrom(in);
+        System.out.println("Result is: " + res.getStr());
+
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
--- SI/In-Class_Multi-Threading/src/main/java/server/SockBaseServer.java
+++ SI/In-Class_Multi-Threading/src/main/java/server/SockBaseServer.java
@@ -2,77 +2,154 @@ package server;
 
 import java.net.*;
 import java.io.*;
+import java.util.Random;
+import java.util.concurrent.locks.*;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import buffers.OperationProtos.Request;
 import buffers.OperationProtos.Response;
 
+class SockBaseServer implements Runnable {
 
-class SockBaseServer {
-    public static void main (String args[]) throws Exception {
+  // Properties for each runnable
+  protected Socket clientSocket = null;
+  protected int id;
+  protected ReadWriteLock mutex;
 
-        int count = 0;
-        ServerSocket    serv = null;
-        InputStream in = null;
-        OutputStream out = null;
-        Socket clientSocket = null;
-        int port = 9099; // default port
-        int sleepDelay = 10000; // default delay
-        if (args.length != 2) {
-          System.out.println("Expected arguments: <port(int)> <delay(int)>");
-          System.exit(1);
-		}
-        
-        try {
-          port = Integer.parseInt(args[0]);
-          sleepDelay = Integer.parseInt(args[1]);
-        } catch (NumberFormatException nfe) {
-          System.out.println("[Port|sleepDelay] must be an integer");
-          System.exit(2);
-        }
-        try {
-            serv = new ServerSocket(port);
-        } catch(Exception e) {
-          e.printStackTrace();
-          System.exit(2);
-        }
-        while (serv.isBound() && !serv.isClosed()) {
-            System.out.println("Ready...");
-            try {
-                clientSocket = serv.accept();
-                // spin up threak
-                in = clientSocket.getInputStream();
-                out = clientSocket.getOutputStream();
-                Request req = Request.parseDelimitedFrom(in);
-                System.out.println(req.toString());
-                int add = 0;
+  // A static string that all threads can access
+  public static String sharedString = "";
 
+  // Constructor takes and sets a client socket, an ID, and a lock
+  public SockBaseServer(Socket clientSocket, int id, ReadWriteLock mutex) {
+    this.clientSocket = clientSocket;
+    this.id = id;
+    this.mutex = mutex;
+  }
 
-                if (req.getOperationType() == Request.OperationType.ADD) {
-                  for (int num: req.getNumsList()){
-                    add += num;
-                  }
-                } else if (req.getOperationType() == Request.OperationType.SUB){
+  // Concatenates a string 5 times (with a random short delay) to the static sharedString
+  // Meant to simulate race conditions and shared memory access. Connect two clients to this server
+  // and send a string close to the same time
+  public void addToSharedString(String str) throws Exception {
+    Random rand = new Random();
+    for (int i = 0; i < 5; i++) {
+      int randNum = rand.nextInt(2 - 1 + 1) + 1;
+      Thread.sleep(1000 * randNum);
+      sharedString += str;
+    }
+  }
 
-                }
+  // This is a synchronized static method version of the above function. It is synchronized on the class object
+  // that this synchronized method belongs to (SockBaseServer.class). Behaves similarly to synchronized block in
+  // run() below.
+  public synchronized static void staticSyncAddToSharedString(String str) throws Exception {
+    Random rand = new Random();
+    for (int i = 0; i < 5; i++) {
+      int randNum = rand.nextInt(2 - 1 + 1) + 1;
+      Thread.sleep(1000 * randNum);
+      sharedString += str;
+    }
+  }
 
-                Response.Builder resBuilder = Response.newBuilder();
-                resBuilder.setSuccess(true);
-                resBuilder.setResult(add);
-                Response res = resBuilder.build();
+  // This is what gets called when we do thread.start()
+  public void run() {
+    try {
+      InputStream in = clientSocket.getInputStream(); // setup our in/out streams
+      OutputStream out = clientSocket.getOutputStream();
+      while (true) {
+        Request req = Request.parseDelimitedFrom(in); // get a request
+        System.out.println("String from client " + id + ":\n" + req.getStr());
 
-                res.writeDelimitedTo(out);
+        /* IF WE WERE TO SIMPLY CALL addToSharedString(), we may have a race condition */
+        // addToSharedString(req.getStr());
 
+        /* This shows how you could used a synchronized block within an instance
+        method, synchronized on the SockBaseServer class
+        There are other ways to handle monitors! Check out:
+        http://tutorials.jenkov.com/java-concurrency/synchronized.html
+        for a good summary */
+        // synchronized(SockBaseServer.class) {
+        //   addToSharedString(req.getStr());
+        // }
 
-                
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            } finally {
-                if (out != null)  out.close();
-                if (in != null)   in.close();
-                if (clientSocket != null) clientSocket.close();
-            }
-        }
+        /* Using a static synchronized method. Behaves similarly to the above block */
+        staticSyncAddToSharedString(req.getStr());
+
+        /* Showing a ReadWriteLock */
+        // mutex.writeLock().lock();
+        // mutex.readLock().lock();
+        // try {
+        //   addToSharedString(req.getStr());
+        // } catch (Exception ex){
+        //   System.out.println(ex);
+        // } finally { // make sure to unlock in a finally block or you may encounter deadlocks or starvation!
+        //   mutex.writeLock().unlock();
+        //   mutex.readLock().unlock();
+        // }
+
+        // Build our response and send it back to the client
+        Response.Builder resBuilder = Response.newBuilder();
+        resBuilder.setSuccess(true);
+        /* note that we are still reading sharedString outside of a lock/synchronized block! might get a "dirty" read!
+        to fix, we could make a static sychronized getSharedString() method, move this into a synchronized block that
+        is synchronized on SockBaseServer.class, or move this call in between a mutex.readLock().Lock() and mutex.readLock.Unlock() */
+        resBuilder.setStr(sharedString);
+        Response res = resBuilder.build();
+        res.writeDelimitedTo(out);
+      }
+    } catch (Exception ex) {
+      System.out.println(ex);
+    }
+  }
+
+  // Main server code
+  public static void main (String args[]) throws Exception {
+    // Parse command line arguments
+    ServerSocket serv = null;
+    int port = 8007; // default port
+    int sleepDelay = 10000; // default delay
+    if (args.length != 2) {
+      System.out.println("Expected arguments: <port(int)> <delay(int)>");
+      System.exit(1);
+    }
+    try {
+      port = Integer.parseInt(args[0]);
+      sleepDelay = Integer.parseInt(args[1]);
+    } catch (NumberFormatException nfe) {
+      System.out.println("[Port|sleepDelay] must be an integer");
+      System.exit(2);
+    }
+    try {
+      serv = new ServerSocket(port);
+    } catch(Exception e) {
+      e.printStackTrace();
+      System.exit(2);
     }
 
-}
+    // Creating a ReadWriteLock to pass to threads
+    ReadWriteLock mutex = new ReentrantReadWriteLock();
 
+    // Creat a count for keeping track of client IDs
+    int count = 0;
+
+    // Begin listening for connections and spinning up threads
+    while (serv.isBound() && !serv.isClosed()) {
+      System.out.println("Ready...");
+      try {
+        Socket clientSocket = serv.accept();
+
+        /* Implementing runnable should look something like this */
+        SockBaseServer runnable = new SockBaseServer(clientSocket, ++count, mutex);
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+        /* Extending thread might look more like this */
+        // SockBaseServer sockBaseServerThread = new SockBaseServer(clientSocket, ++count, mutex);
+        // sockBaseServerThread.start();
+
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+    }
+  }
+}
